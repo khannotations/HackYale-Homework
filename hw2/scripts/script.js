dd93eec7d03c4a324ec641ec04403ee67abf5fb8
Rafi Khan <rafi.fa.khan@gmail.com>
2013-10-08 14:48:52 -0400

    script with jquery

--- hw2/scripts/script.js
+++ hw2/scripts/script.js
@@ -1,10 +1,11 @@
-window.onload = function() {
+window.onload = function() {          // Wait for DOM to load.
   var p = document.getElementById("my-p");
-  console.log(p);
+  console.log(p);                     // Returns just one element
   p.style.color = "red";
   p.style.backgroundColor = "blue";
+  p.innerHTML = "Something funky!!"
 
-  var lis = document.getElementsByTagName("li");
+  var lis = document.getElementsByTagName("li"); // An array
 
   for(var index=0; index<lis.length; index++) {
     var li = lis[index];
@@ -14,4 +15,4 @@ window.onload = function() {
 
   var response = prompt("Red, green or blue?");
   console.log(response);
-}
+};                                    // Don't forget semicolon!
---
+++ hw2/scripts/script_jquery.js
@@ -0,0 +1,17 @@
+$(document).ready(function() {
+  var p = $("#my-p");
+  console.log(p);
+  $(p).css({color: "red", backgroundColor: "blue"});
+  $(p).html("Something funky with jQuery!");
+
+  var lis = $("li");
+  for(var index=0; index<lis.length; index++) {
+    var li = lis[index];
+    var color = Math.floor((256.0/lis.length) * index);
+    $(li).css("backgroundColor",
+      "rgb("+color+","+color+","+color+")");
+  }
+
+  var response = prompt("jQuery, or plain Javascript?");
+  console.log(response);
+});
